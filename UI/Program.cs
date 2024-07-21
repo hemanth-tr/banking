2cd35e201c2e2b7c746930dc89644ed0f0662a0b
Hemanth <trhemanth2@gmail.com>
2024-07-21 15:11:03 +0530

    adding areas

--- UI/Program.cs
+++ UI/Program.cs
@@ -30,6 +30,14 @@ public class Program
             name: "default",
             pattern: "{controller=Home}/{action=Index}/{id?}");
 
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+              name: "areas",
+              pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+            );
+        });
+
         app.Run();
     }
 }
